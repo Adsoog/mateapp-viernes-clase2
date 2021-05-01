36b78945f6e31c2a868870de961c7f134ee0fffb
Adsoog <iadso.ao@gmail.com>
2021-04-30 21:15:42 -0500

    Hola mundo

---
+++ Hola mundo.py
@@ -0,0 +1,48 @@
+print("Hola mundo")
+edad = "20"
+print(edad)
+print("tengo", edad, "chillonarios años")
+
+base = 10
+height = 3
+
+area = base * height/2
+print("El area es:" ,area)
+
+#suma
+a = 4
+b = 3
+suma = a + b
+print("la suma es" ,suma)
+
+#resta
+resta = a - b
+print("la mugrosa resta es" ,resta)
+
+
+
+
+
+x = 4 
+y = 5
+w = 3
+z = 2
+
+#la solucion
+asdadasdasdasd = ((x+y)*w)/2
+print("la solucion es" ,asdadasdasdasd)
+
+campanito = "a"
+canalla = "b"
+
+resultado = campanito + canalla
+
+print(resultado)
+print(resultado * 5)
+
+caracteres = len("Hola mundo")
+print(caracteres)
+
+name = input("Cual es tu nombre" )
+print("Hola ", name)
+print("Hola "+ name)
